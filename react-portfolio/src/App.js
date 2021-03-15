80c50d2a8bb7d13a82bf03e6838d4dca19f7490f
Uyen Nguyen <udnguyen@ucsd.edu>
2021-03-15 01:52:53 -0700

    edit routes for gh pages deploy again

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,24 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import Portfolio from "./pages/Portfolio";
-import Resume from "./pages/Resume";
-import Contact from "./pages/Contact";
-
+import { BrowserRouter as Router } from "react-router-dom";
 import AllPages from "./pages/AllPages";
 
 function App() {
     return (
         <Router basename={process.env.PUBLIC_URL}>
-            <div>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/portfolio" component={Portfolio} />
-                    <Route exact path="/resume" component={Resume} />
-                    <Route exact path="/contact" component={Contact} />
-                </Switch>
-                <AllPages />
-            </div>
+            <AllPages />
         </Router>
     );
 };
